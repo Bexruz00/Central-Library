2e8cb0ebbeee4915a7b89ae016b0f87520d8efc0
Bexruz00 <barnoyevbexruz140@gmail.com>
2025-05-22 02:27:43 +0500

    responsive and others

--- modules/Hero/index.tsx
+++ modules/Hero/index.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 const Hero = () => {
   const t = useTranslations("Hero")
   return (
-    <div className='hero md:!py-[68px] !py-[50px]'>
+    <div className='hero md:!py-[68px] sm:!py-[50px] !py-[30px]'>
       <div className='containers flex flex-col items-center'>
         <h1 className='hero__title'>{t("hero-title")}</h1>
         <p className='lg:text-[20px] md:text-[18px] text-[16px] font-normal text-white max-w-[888px] text-center md:!mt-[30px] !mt-[20px] md:!mb-[42px] !mb-[38px]'>{t("hero-subtitle")}</p>
         <div className='flex items-center gap-[10px] rounded-md !p-1 border border-[#CC9600]'>
-          <input type="text" className='bg-transparent text-[#A59F9F] text-[15px] !pl-[36px] md:!max-w-[255px] max-w-[250px] md:h-[50px] h-[30px] outline-0 rounded-md' placeholder={t("hero-input")}/>
+          <input type="text" className='bg-transparent text-[#A59F9F] text-[15px] !pl-[36px] md:!max-w-[255px] sm:max-w-[240px] max-w-[200px] md:h-[50px] h-[30px] outline-0 rounded-md' placeholder={t("hero-input")}/>
           <button className='md:!px-[27px] !px-[25px] md:!py-[11px] !py-[9px] bg-[#4B330B] rounded-md text-white text-[18px]'>{t("hero-button")}</button>
         </div>
       </div>
--- modules/Swippers/index.tsx
+++ modules/Swippers/index.tsx
@@ -1,13 +1,18 @@
 "use client"
 import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css/pagination'
 import 'swiper/css'
 import { instance } from '@/hooks/instance'
+import { useTranslations } from 'next-intl'
 
 const Swippers = () => {
+  const t = useTranslations("Swippers")
+
   const [books, setBooks] = useState<any[]>([])
+  const router = useRouter()
 
   useEffect(() => {
     const AllBooks = async () => {
@@ -22,21 +27,36 @@ const Swippers = () => {
     AllBooks()
   }, [])
 
+  const handleClick = (id: number) => {
+    router.push(`/books/${id}`)
+  }
+
   return (
-    <div className='containers'>
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        loop={true}
-        pagination={{ clickable: true }}
-        navigation={true}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {books.map((item: any, index: number) => (
-          <SwiperSlide key={index} className='!h-[500px] bg-red-200'>
-            <img src={item.book_image} alt="books" />
-            {item.book_name}
+    <div className='containers rounded-md md:!mt-[100px] sm:!mt-[50px] !mt-[10px]'>
+      <h2 className='md:text-[48px] sm:text-[40px] text-[32px] text-center font-bold md:!mb-[50px] sm:!mb-[10px] !mb-[30px]'>{t("swippers-title")}</h2>
+      <Swiper slidesPerView={4} spaceBetween={20} loop={true} pagination={{ dynamicBullets: true, }} navigation={true} modules={[Pagination]} className="swipper1">
+        {books.slice(0, 6).map((item: any, index: number) => (
+          <SwiperSlide key={index} className='rounded-md lg:!h-[450px] !h-[380px] !p-3 cursor-pointer duration-300'>
+            <div onClick={() => handleClick(item.id)} className='overflow-hidden flex gap-[16px] justify-center hover:scale-102 duration-200 flex-col max-w-[230px] rounded-md'>
+              <img className='w-[220px] rounded-md ' src={item.book_image} alt="books" />
+              <span>
+                <p className='text-[20px] font-bold !pb-[8px]'>{item.book_name}</p>
+                <strong className='font-normal text-[15px] '>{item.book_author}</strong>
+              </span>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper slidesPerView={2} spaceBetween={20} loop={true} pagination={{ dynamicBullets: true, }} navigation={true} modules={[Pagination]} className="swipper2">
+        {books.slice(0, 6).map((item: any, index: number) => (
+          <SwiperSlide key={index} className='!flex !items-center !justify-center rounded-md sm:!h-[450px] !pb-[50px] !h-[330px] sm:!p-3 !p-0 cursor-pointer duration-300'>
+            <div onClick={() => handleClick(item.id)} className=' overflow-hidden flex gap-[16px] justify-center hover:scale-102 duration-200 flex-col max-w-[230px] rounded-md'>
+              <img className='w-[220px] rounded-md ' src={item.book_image} alt="books" />
+              <span>
+                <p className='text-[20px] font-bold !pb-[8px]'>{item.book_name}</p>
+                <strong className='font-normal text-[15px] '>{item.book_author}</strong>
+              </span>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
